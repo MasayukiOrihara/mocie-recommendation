85b5e4790290441a945cd3b2711606aa90fbaa96
NPWE164\m_orihara <m_orihara@freestyles.jp>
2025-04-17 17:46:26 +0900

    「05. 動的な映画 ID ページ」の変更分

--- src/components/movie-poster.tsx
+++ src/components/movie-poster.tsx
@@ -1,16 +1,19 @@
 import Image from 'next/image';
 import Link from 'next/link';
  
+import { cn } from '@/lib/utils';
 import { Movie, SimilarMovie } from '@/types';
  
 function MoviePoster({
   index,
   similarityRating,
   movie,
+  isSimilarity,
 }: {
   index?: number;
   similarityRating?: number;
   movie: Movie | SimilarMovie;
+  isSimilarity?: boolean;
 }) {
   return (
     <Link
@@ -24,25 +27,35 @@ function MoviePoster({
           height={450}
           src={movie.Poster}
           alt={movie.Title}
-          className="min-w-64 max-w-64 object-contain rounded-lg shadow-lg"
+          className={cn(
+            'object-contain rounded-lg shadow-lg',
+            isSimilarity && '-ml-28 min-w-32 max-w-32',
+            !isSimilarity && 'min-w-64 max-w-64',
+          )}
         />
       </div>
       <div className="relative">
         {similarityRating && (
-          <div className="absolute w-14 h-14 flex items-center justify-center bottom-0 right-0 bg-orange-300 bg-opacity-90 p-2 rounded-full m-5 font-bold">
+          <div className="absolute -left-12 w-14 h-14 flex items-center justify-center bottom-0 right-0 bg-orange-300 bg-opacity-90 p-2 rounded-full m-5 font-bold">
             {similarityRating}%
           </div>
         )}
  
         {index && (
-          <div className="absolute text-gray-100 top-18 -left-4 text-6xl font-extrabold drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+          <div className="absolute text-white/40 -top-48 left-8 text-9xl font-extrabold drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.2)]">
             {index}
           </div>
         )}
       </div>
  
       <div className="px-4 py-2">
-        <p className="text-lg font-semibold line-clamp-1 w-64">
+        <p
+          className={cn(
+            'text-lg font-semibold line-clamp-1',
+            isSimilarity && 'w-32',
+            !isSimilarity && 'min-w-64 max-w-64',
+          )}
+        >
           {movie.Title}
         </p>
         <p className="text-gray-500 line-clamp-1">{movie.Genre}</p>
